e9d989abef3fd1a6c97fb98fce28c6eba8fe396c
Axushhh <insta.mangi987@gmail.com>
2024-03-21 16:53:14 +0530

    commit

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,13 +1,16 @@
 import FirstFunc from "./first"
 
 function App() {
+  const username = "coding practice"
   return (
     <>
       <FirstFunc/>
-      <h2>the app is called</h2>
+      <h2>the app is called {username}</h2> 
       <h3>working with vite</h3>
     </>
     )
 }
 
 export default App
+
+// {username} this is called evaluated expresion (it means final outcome)
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,6 +2,42 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-      <App />
-)
+function MyApp(){
+      return (
+          <div>
+              <h1>Custom App | chai</h1>
+          </div>
+      )
+  }
+  // here we tried to inject our element but failed
+
+  // const ReactElement = {
+  //     type: 'a',
+  //     props: {
+  //         href: 'https://google.com',
+  //         target: '_blank'
+  //     },
+  //     children: 'Click me to visit google'
+  // }
+  
+  // by this method we can inject by writing the whole html code
+  const anotherElement = (
+      <a href="https://google.com" target='_blank'>Visit google</a>
+  )
+  
+  
+  
+  const anotherUser = " react working"
+  
+  const reactElement = React.createElement(
+      'a', // type, keys
+      {href: 'https://google.com',target: '_blank' }, /// ref
+      'click me to visit google',
+      anotherUser // evaluated expression (child/props)
+  )
+  
+  ReactDOM.createRoot(document.getElementById('root')).render(
+   
+      reactElement
+    
+  )
