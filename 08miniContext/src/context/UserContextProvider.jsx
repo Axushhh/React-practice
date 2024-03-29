0e29ebc4988717494989242275a6c0a2637646ae
Axushhh <insta.mangi987@gmail.com>
2024-03-29 20:01:56 +0530

    commit

--- 08miniContext/src/context/UserContextProvider.jsx
+++ 08miniContext/src/context/UserContextProvider.jsx
@@ -3,7 +3,7 @@ import UserContext from "./UserContext";
 import { useState } from "react";
 
 const UserContextProvider = ({children}) => {
-    const [user, setUser] = useState(null)
+    const [user, setUser] = useState(null) // user is variable and setUser is method 
     return(
         // in value user is passed to take value while setUser is used to add value in this state
         <UserContext.Provider value={{user, setUser}}> 
